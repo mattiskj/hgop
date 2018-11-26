f360263c3b2488e9ccc2f38383b6a142cbbf8997
GunnlaugurG <gunnlaugur16@ru.is>
2018-11-26 11:51:48 +0000

    working script

--- scripts/verify_environment.sh
+++ scripts/verify_environment.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-
+echo `date`
 echo Hello $USER
+echo
+echo
 echo System `uname -a`
+echo
+echo
+echo node version  `node --version`
+echo `git --version`
+echo npm version `npm --version`
+echo `date`
