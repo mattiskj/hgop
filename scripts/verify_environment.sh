8eb374267e64a8eb94a2091b257c96b27ac24dce
GunnlaugurG <gunnlaugur16@ru.is>
2018-11-26 12:44:07 +0000

    Assignment 1 complete

--- scripts/verify_environment.sh
+++ scripts/verify_environment.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+rm -f "logfile.txt"
+getInfo(){
+	echo starting time `date`
+	echo Hello $USER
+	echo
 
-echo `date`
-echo Hello $USER
-echo
-echo
-echo System `uname -a`
-echo
-echo
-echo node version  `node --version`
-echo `git --version`
-echo npm version `npm --version`
-echo `date`
+	#get the system info
+	echo System `uname -mrs`
+	echo
+
+	#get versions of git, npm, node
+	echo node version  `node --version`
+	echo `git --version`
+	echo npm version `npm --version`
+
+	#displaying the end time
+	echo end time `date`
+}
+getInfo | tee -a "logfile.txt"
+
