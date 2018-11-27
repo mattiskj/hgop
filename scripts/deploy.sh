4ff5e0b091baae0acc0a0bdcef3d2c38796b0958
GunnlaugurG <gunnlaugur16@ru.is>
2018-11-27 16:50:17 +0000

    script for deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+`cd ..`
 echo Destroying any running terraform.
-`terraform destroy -auto-approve`
+terraform destroy -auto-approve
 
 echo Creating new terraform Instance.
-`terraform apply -auto-approve`
+terraform apply -auto-approve
 
-Running initialization script
-`ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"`
+echo Running initialization script
+ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"
 
