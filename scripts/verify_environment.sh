dd610e0833353d51ed35ba2d386ef847f8a26f02
GunnlaugurG <gunnlaugur16@ru.is>
2018-11-27 12:38:39 +0000

    Added infastructure and updated script

--- scripts/verify_environment.sh
+++ scripts/verify_environment.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 rm -f "logfile.txt"
 getInfo(){
-	echo starting time `date`
+	
 	echo Hello $USER
 	echo
-
+	echo starting time `date`
+	echo
 	#get the system info
 	echo System `uname -mrs`
 	echo
 
-	#get versions of git, npm, node
-	echo node version  `node --version`
-	echo `git --version`
-	echo npm version `npm --version`
+	#get versions of dependencies
+	gitVersion=$(git --version)
+	npmVersion=$(npm --version)
+	nodeVersion=$(node --version)
+	awsCli=$(aws --version)
+	terraformVersion=$(terraform --version)
+	echo node version: $nodeVersion
+	echo git version : $gitVersion
+	echo npm version : $npmVersion
+	echo aws version : $awsCli
+	echo terraform version: $terraformVersion
+
 
 	#displaying the end time
 	echo end time `date`
 }
 getInfo | tee -a "logfile.txt"
-
