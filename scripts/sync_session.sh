b2269c61b832cb0e52039c777d9d36f231ecd34d
GunnlaugurG <gunnlaugur16@ru.is>
2018-12-05 15:48:23 +0000

    sync_session added

---
+++ scripts/sync_session.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+JENKINS_URL= "http://ec2-54-226-220-170.compute-1.amazonaws.com:8080"
+
+scp -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ~/.aws/credentials ubuntu@${JENKINS_URL}:~/credentials
+ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo mv ~/credentials /var/lib/jenkins/.aws/credentials"
+ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo chmod a+r /var/lib/jenkins/.aws/credentials"
