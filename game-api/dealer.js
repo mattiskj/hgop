6269c1eb46a11aacb9037a237c02e012dcfc6264
GunnlaugurG <gunnlaugur16@ru.is>
2018-12-06 16:14:59 +0000

    eslint fix

--- game-api/dealer.js
+++ game-api/dealer.js
@@ -1,8 +1,10 @@
-module.exports = () => {
+// dealer.js
+module.exports = (context) => {
   return {
-    shuffle: (deck, random) => {
+    shuffle: (deck) => {
+      const random = context('random');
       for (let i = 0; i < deck.length - 1; i++) {
-        const j = Math.floor(random() * (deck.length - i)) + i;
+        const j = Math.floor(random() * (deck.length - i));
         const card = deck[j];
         const old = deck[i];
         deck[i] = card;
