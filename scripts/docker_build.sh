d979473fb0bfaa354d9808d7358daca19a855195
GunnlaugurG <gunnlaugur16@ru.is>
2018-12-07 16:29:13 +0000

    Added todo for scripts

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,6 +3,6 @@
 GIT_COMMIT=$1
 
 cd game-api
-docker build -t mattiskj/hgop:$GIT_COMMIT .
+docker build -t mattiskj/hgop:$GIT_COMMIT . || exit 1
 
 # TODO exit on error if any command fails
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -2,6 +2,6 @@
 
 GIT_COMMIT=$1
 
-docker push mattiskj/hgop:$GIT_COMMIT
+docker push mattiskj/hgop:$GIT_COMMIT || exit 1
 
 # TODO exit on error if any command fails
